694b4e7044f2e7c37609e35bc44424336dce1f8d
Santeri <santeri.volkov@aalto.fi>
2019-01-09 20:37:07 +0200

    added jinja2 backend to settings, and added jinja2.py

--- project/settings.py
+++ project/settings.py
@@ -63,8 +63,14 @@ ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
         'DIRS': [os.path.join(BASE_DIR, 'project/templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {'environment': 'project.jinja2.environment'},
+    },
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
