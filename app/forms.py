10d9de5cdb48e32b00995d0defe5587c8bf5fc54
Santeri Volkov <santeri.volkov@aalto.fi>
2019-01-12 02:32:01 +0200

    Modelupdate

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,29 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 from app.models import Game
 from app.models import Genre
+
+User = get_user_model()
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    dev_registration = forms.BooleanField(required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'dev_registration')
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.email = self.cleaned_data['email']
+        user.is_dev = self.cleaned_data['dev_registration']
+        if commit:
+            user.save()
+        return user
+
 
 class GameForm(forms.ModelForm):
     class Meta:
