30c385140103957a42f4a63543f812404ec13c60
Santeri <santeri.volkov@aalto.fi>
2019-01-13 20:53:57 +0200

    set debug back on

--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import django_heroku
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -29,9 +30,7 @@ SECRET_KEY = 'e-pzjz^rl!f@1*bDvmp=ci!wpgmq6&c(o+mwet*tr^(ap=jy7x'
 # os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# To have debug True create a .env file in project root and add 'DEBUG = True'
-DEBUG = os.environ.get('DEBUG')
-
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
