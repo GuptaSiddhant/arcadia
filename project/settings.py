ea84a4a90f31e39bd9ad720ab4b6f2d69147797e
Santeri <santeri.volkov@aalto.fi>
2019-01-15 14:28:00 +0200

    added some security parameters to settings

--- project/settings.py
+++ project/settings.py
@@ -146,3 +146,15 @@ django_heroku.settings(locals(), test_runner=False)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 AUTH_USER_MODEL = 'app.User'
+
+# Security
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'Strict'
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+X_FRAME_OPTIONS = 'DENY'
+# SECURE_HSTS_SECONDS = 300  # set low, but when site is ready for deployment, set to at least 15768000 (6 months)
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_HSTS_PRELOAD = True
