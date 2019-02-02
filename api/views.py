d3e1e0e00403c941b5882d18e02f64c9c28178b3
Henri Ahti <henriahti@Henris-MacBook-Pro.local>
2019-02-02 11:21:03 +0200

    Django Rest Framework with session based authentication

---
+++ api/views.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from app.models import Game, Genre, Transaction, GameState, GameScore
+from api.serializers import UserSerializer, GameSerializer, GenreSerializer, \
+    TransactionSerializer, GameStateSerializer, GameScoreSerializer
+
+from rest_framework import viewsets
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class GameStateViewSet(viewsets.ModelViewSet):
+    queryset = GameState.objects.all()
+    serializer_class = GameStateSerializer
+
+class GameScoreViewSet(viewsets.ModelViewSet):
+    queryset = GameScore.objects.all()
+    serializer_class = GameScoreSerializer
+
+
+
+
+
